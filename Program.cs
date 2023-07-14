ca8fc5eee600b3bf426eca1f79006597a2bcfe71
sand2104 <prasadsandy2000@gmail.com>
2023-07-14 12:27:16 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayexlistex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList hardwareList = new ArrayList();
+            hardwareList.Add("CD");
+            hardwareList.Add("Printer");
+            hardwareList.Add("KeyBoard");
+            hardwareList.Add("Mouse");
+            hardwareList.Add("Network Card");
+            Console.WriteLine("Intial Hardware List");
+
+            for (int i = 0; i < hardwareList.Count; i++)
+            {
+                Console.WriteLine(hardwareList[i]);
+            }
+            Console.WriteLine("Enter item to search & update");
+            string searchItem = Console.ReadLine();
+            int index = hardwareList.IndexOf(searchItem);
+            if (index == -1)
+            {
+                Console.WriteLine($"No Such item {searchItem} exist in hardware list");
+            }
+            else
+            {
+                Console.WriteLine("Enter update item");
+                hardwareList[index] = Console.ReadLine();
+                Console.WriteLine("Update Hardware List is as follows!!!");
+                for (int i = 0; i < hardwareList.Count; i++)
+                {
+                    Console.WriteLine(hardwareList[i]);
+                }
+            }
+        }
+    }
+}
+     
